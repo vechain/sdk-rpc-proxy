f890adf41e5bfeb6fcaf68ea83c6ce7cb1b2be11
clayton neal <clayton.neal@vechain.org>
2025-03-26 14:53:40 +0000

    bootstrap repo

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    coverageReporters: ['html', 'lcov', 'json'],
+    reporters: ['default', 'jest-junit'],
+    workerThreads: true,
+    testTimeout: 120_0000,
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100
+        }
+    }
+};
---
+++ src/utils/args/index.ts
@@ -0,0 +1,3 @@
+export * from './args-options';
+export * from './args-parser';
+export * from './env-to-args';
